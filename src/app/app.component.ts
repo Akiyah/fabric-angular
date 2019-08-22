66ccd828114d569b94e9a45bd7b1e7c6b5e741b9
Akiyah <akiya.mizukoshi@gmail.com>
2019-08-22 18:22:45 +0900

    add canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fabric-angular';
+
+  private canvas: any;
+
+  ngOnInit(): void {
+    this.canvas = new fabric.Canvas('canvas', {
+      isDrawingMode: true
+    });
+  }
 }
